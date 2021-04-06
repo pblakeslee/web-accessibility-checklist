e69a50d4d2af6a21edd1b44e541addda1c33984e
pbla2394@gmail.com <pbla2394@gmail.com>
2021-04-06 11:28:56 -0400

    added section 4 and 5 of component

--- src/app/task-parent/task-parent.component.ts
+++ src/app/task-parent/task-parent.component.ts
@@ -156,5 +156,138 @@ export class TaskParentComponent implements OnInit {
     },
   ];
 
+  understandables: Task[] = [
+    {
+      sectionTitle: 'understandable',
+      section: 'Page titles (2.4.2): ',
+      message:
+        'Each page of a website needs to have a unique and descriptive page title.',
+      completed: false,
+    },
+    {
+      sectionTitle: 'understandable',
+      section: 'Focus order (2.4.3):',
+      message:
+        'Users must be able to navigate through a website in a logical sequential order that preserves meaning.',
+      completed: false,
+    },
+
+    {
+      sectionTitle: 'understandable',
+      section: 'Link anchor text (2.4.4):',
+      message:
+        'The purpose of each link should be clear based on its anchor text (e.g. don’t use “click here”).',
+      completed: false,
+    },
+    {
+      sectionTitle: 'understandable',
+      section: 'Multiple ways (2.4.5):',
+      message:
+        'There are multiple ways to access different pages/information on a website (e.g. search bar, nav menus, sitemap, breadcrumbs, helpful links after content).',
+      completed: false,
+    },
+    {
+      sectionTitle: 'understandable',
+      section: 'Descriptive headings and labels (2.4.6):',
+      message:
+        'Headings and programmatic labels must be clear and descriptive. They do not need to be lengthy.',
+      completed: false,
+    },
+    {
+      sectionTitle: 'understandable',
+      section: 'Focus indicator (2.4.7):',
+      message:
+        'Any “user interface control” that receives focus from a keyboard user should indicate that focus on the current selected element (e.g. add a visible border around a text link).',
+      completed: false,
+    },
+    {
+      sectionTitle: 'understandable',
+      section: 'Website language (3.1.1):',
+      message: 'Set the language for your website.',
+      completed: false,
+    },
+    {
+      sectionTitle: 'understandable',
+      section: 'Language changes (3.1.2):',
+      message:
+        'Indicate any language changes for an entire page or within the content.',
+      completed: false,
+    },
+  ];
+
+  predictabilities: Task[] = [
+    {
+      sectionTitle: 'predictability',
+      section: 'No focus change (3.2.1):',
+      message:
+        'Nothing changes merely because an item receives focus; a user must actively choose to activate an item (e.g. hit enter to submit) before a change takes place.',
+      completed: false,
+    },
+    {
+      sectionTitle: 'predictability',
+      section: 'No input change (3.2.2):',
+      message:
+        'Nothing changes just because information is inputted into a field (e.g. form doesn’t auto submit once all fields are filled out).',
+      completed: false,
+    },
+
+    {
+      sectionTitle: 'predictability',
+      section: 'Consistent navigation (3.2.3):',
+      message:
+        'Keep navigation layout consistent throughout all pages of the website (e.g. same links in the same order).',
+      completed: false,
+    },
+    {
+      sectionTitle: 'predictability',
+      section: 'Consistent identification (3.2.4):',
+      message:
+        'Components that have the same function within a website are identified consistently (but not necessarily identically) (e.g. two check marks can indicate two different things as long as their function is different — one indicates “approved” on one page but “included” on another).',
+      completed: false,
+    },
+    {
+      sectionTitle: 'predictability',
+      section: 'Error identification (3.3.1):',
+      message:
+        'Make any form errors easy to identify, understand, and correct.',
+      completed: false,
+    },
+    {
+      sectionTitle: 'predictability',
+      section: 'Form labels and instructions (3.3.2):',
+      message:
+        'Programmatically label all form or input fields so that a user knows what input and what format is expected.',
+      completed: false,
+    },
+    {
+      sectionTitle: 'predictability',
+      section: 'Error suggestions (3.3.3):',
+      message:
+        'If an input error is automatically detected, then suggestions for correcting the error should be provided.',
+      completed: false,
+    },
+    {
+      sectionTitle: 'predictability',
+      section: 'Error prevention on important forms (3.3.4):',
+      message:
+        'For pages that create legal commitments or financial transactions or any other important data submissions, one of the following is true: 1) submissions are reversible, 2) the user has an opportunity to correct errors, and 3) confirmation is available that allows an opportunity to review and correct before submission.',
+      completed: false,
+    },
+    {
+      sectionTitle: 'predictability',
+      section: 'Parsing (4.1.1):',
+      message:
+        'Make sure HTML code is clean and free of errors, particularly missing bracket closes. Also, make sure all HTML elements are properly nested.',
+      completed: false,
+    },
+    {
+      sectionTitle: 'predictability',
+      section: 'Name, role, value (4.1.2): ',
+      message:
+        'For all user interface components (including forms, links, components generated by scripts), the name, role, and value should all be able to be programmatically determined; make sure components are compatible with assistive technology.',
+      completed: false,
+    },
+  ];
+
   ngOnInit(): void {}
 }
